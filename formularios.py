fa187600a2b020614eb08ad84a185a843e83138b
yzabala <91234510+yzabala@users.noreply.github.com>
2021-10-20 01:37:13 -0500

    Update formularios.py

--- formularios.py
+++ formularios.py
@@ -4,16 +4,12 @@ from wtforms.validators import InputRequired, Length, DataRequired
 from wtforms.fields.html5 import EmailField
 
 class Registro(FlaskForm):
-    nom = TextField('Nombre', validators=[Length(min=5, max=20,message='Longitud fuera de rango'),InputRequired(message='El nombre es requerido')])
-    usu = TextField('Usuario', validators=[Length(min=5, max=40,message='Longitud fuera de rango'),InputRequired(message='Usuario es requerido')])
-    ema = EmailField('E-mail', validators=[Length(min=10, max=20,message='Longitud fuera de rango'),InputRequired(message='E-mail es requerido')])
-    dire = TextField('Dirección', validators=[Length(min=5, max=40,message='Longitud fuera de rango'),InputRequired(message='La dirección es requerida')])
-    tel = TextField('Telefono', validators=[Length(min=7, max=40,message='Longitud fuera de rango'),InputRequired(message='El telefóno es requerido')])
-    tipoU = TextField('Tipo U', validators=[Length(min=7, max=40,message='Longitud fuera de rango'),InputRequired(message='El tipo de Usuario es requerido')])
+    nom = TextField('Nombre', validators=[Length(min=2, max=100,message='Longitud fuera de rango'),InputRequired(message='El nombre es requerido')])
+    ema = EmailField('E-mail', validators=[Length(min=2, max=100,message='Longitud fuera de rango'),InputRequired(message='E-mail es requerido')])
+    pas = PasswordField('Password', validators=[Length(min=2, max=100,message='Longitud fuera de rango'),InputRequired(message='Password es requerido')])
+    dire = TextField('Dirección', validators=[Length(min=2, max=100,message='Longitud fuera de rango'),InputRequired(message='La dirección es requerida')])
+    tel = TextField('Telefono', validators=[Length(min=2, max=100,message='Longitud fuera de rango'),InputRequired(message='El telefóno es requerido')])
+    tipoU = TextField('Tipo U', validators=[Length(min=2, max=100,message='Longitud fuera de rango'),InputRequired(message='El tipo de Usuario es requerido')])
     regbtn = SubmitField('Registrarse')
 
-class Login(FlaskForm):
-    usr = TextField('Usuario *',validators=[Length(min=5, max=40, message='Longitud fuera de rango'),InputRequired(message='Usuario es requerido')])
-    email = EmailField('Email *',validators=[Length(min=5, max=40, message='Longitud fuera de rango'),DataRequired(message='Email es requerido')])
-    btn = SubmitField('Ingresar')
 
